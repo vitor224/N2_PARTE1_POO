42cfff399169a51e3393ae14af616634dc34dbd4
vitor224 <vitordonascimento714@gmail.com>
2023-05-28 01:15:35 -0300

    N2_Parte01_POO

--- src/main/java/com/example/ContactManager/Contact.java
+++ src/main/java/com/example/ContactManager/Contact.java
@@ -2,6 +2,32 @@ package com.example.ContactManager;
 
 public class Contact {
     private String name;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Contact() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     private String phoneNumber;
 
     // Construtor, getters e setters
--- src/main/java/com/example/ContactManager/ContactController.java
+++ src/main/java/com/example/ContactManager/ContactController.java
@@ -2,26 +2,45 @@ package com.example.ContactManager;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class ContactController {
-    private Map<String, String> contacts = new HashMap<>();
+    private Map<String, Contact> contacts = new HashMap<>();
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("contacts", contacts);
-        return "index";
+    public String listContacts(Model model) {
+        model.addAttribute("contacts", contacts.values());
+        return "list";
     }
 
-    @PostMapping("/addContact")
-    public String addContact(@RequestParam String name, @RequestParam String phoneNumber) {
-        contacts.put(name, phoneNumber);
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("contact", new Contact());
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addContact(@ModelAttribute Contact contact) {
+        contacts.put(contact.getName(), contact);
         return "redirect:/";
     }
+
+    @GetMapping("/search")
+    public String searchContact(@RequestParam String name, Model model) {
+        Contact contact = contacts.get(name);
+        model.addAttribute("contacts", (contact != null) ? Collections.singletonList(contact) : contacts.values());
+        return "list";
+    }
+
+    @GetMapping("/delete/{name}")
+    public String deleteContact(@PathVariable String name) {
+        contacts.remove(name);
+        return "redirect:/";
+    }
+
 }
